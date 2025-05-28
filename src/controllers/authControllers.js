d4d05cbf1b88a7afa0c70a9f77575589fa7a51de
dwitamara <septyaatid02@gmail.com>
2025-05-28 11:01:45 +0700

    add nomor

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -107,6 +107,7 @@ export const login = async (req, res) => {
         id: user.id,
         username: user.username,
         email: user.email,
+        nomor: user.nomor
       },
     });
   } catch (error) {
